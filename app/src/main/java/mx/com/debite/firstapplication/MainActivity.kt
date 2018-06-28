cd1bcd7efa35fa726c2ce4db2e59bb8abae214aa
CharlieSD <carlos.espinoza@debite.com.mx>
2018-06-28 16:21:10 -0500

    Lifecycle

--- app/src/main/java/mx/com/debite/firstapplication/MainActivity.kt
+++ app/src/main/java/mx/com/debite/firstapplication/MainActivity.kt
@@ -2,11 +2,43 @@ package mx.com.debite.firstapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this,"Hola soy Kotlin", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this,"LLamada a función onStart", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this,"LLamada a función onResume", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this,"LLamada a función onPause", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this,"LLamada a función onStop", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this,"LLamada a función onRestart", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this,"LLamada a función onDestroy", Toast.LENGTH_SHORT).show()
     }
 }
